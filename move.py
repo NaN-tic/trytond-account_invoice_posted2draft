d886a8d58a11b87ca1361e344da30b84b2261e84
Bernat Brunet <bernat@nan-tic.com>
2025-08-21 18:35:47 +0200

    Remove model Invoice from the control of the draft button, because it's in the account_es module.

--- move.py
+++ move.py
@@ -18,12 +18,11 @@ class Move(metaclass=PoolMeta):
         return super().check_modify(*args, **kwargs)
 
     def get_allow_draft(self, name):
-        Invoice = Pool().get('account.invoice')
         Move = Pool().get('account.move')
 
         result = super().get_allow_draft(name)
 
-        if self.origin and isinstance(self.origin, (Invoice, Move)):
+        if self.origin and isinstance(self.origin, Move):
             return True
         return result
 
